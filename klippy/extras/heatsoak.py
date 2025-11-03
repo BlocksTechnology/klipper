b2fb505c8ef7fd43067ddeceadf19c08858de0f2
HugoCLSC <hugo.santos.costa@gmail.com>
2025-11-03 19:13:32 +0000

    initial heatsoak commit

---
+++ klippy/extras/heatsoak.py
@@ -0,0 +1,30 @@
+import logging
+import typing
+
+
+class Heatsoak:
+    def __init__(self, config) -> None:
+        self.printer = config.get_printer()
+        self.reactor = self.printer.get_reactor()
+        self.gcode = self.printer.lookup_object("gcode")
+        self.state = "idle"
+
+        self.heater = config.get("heater", "heater_bed")
+        if config.get("temperature_sensor", None):
+            self.temperature_sensor = config.get("temperature_sensor")
+
+        self.gcode.register_command(
+            "HEATSOAK", self.cmd_HEATSOAK, "Heat and stabilize chamber temperature"
+        )
+        self.stable_time: int = config.getint(
+            "default_stable_time", default=20, minval=15, maxval=120
+        ) # In minutes
+        self.stabilize_timer = self.reactor.register_timer(
+            self.stabilize_check, self.reactor.NEVER
+        )
+
+    def stabilize_check(self): ...
+    def cmd_HEATSOAK(self, gcmd): ...
+
+    def get_status(self):
+        return {"state": self._state}
